c88250f75f837ee3a7bb5a05e1aa291556c1a14c
koori <ice930ice@gmail.com>
2024-04-16 11:11:24 +0900

    関数の基礎までは完了(p124)　ｽﾀｲﾙは抜かし、155Pからは別ﾌﾞﾗﾝﾁ

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import {useState} from "react";
+import {useEffect} from "react";
 import {ColoredMessage}from "./components/ColoredMessage";
 
 export const App = () => {
+  console.log("ﾚﾝﾀﾞﾘﾝｸﾞ")
+
+  const [num,setNum] = useState(0);
   const onClickButton = () => {
-    alert();
+    setNum((prev) => prev +1);
   };
+
+  useEffect(() => {
+    alert("更新");},[num]);
 
    return (
     <div>
       <h1 style={{color:"red"}}>こんにちは</h1>
       <ColoredMessage/>
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-      <ColoredMessage color="pink">元気です！</ColoredMessage>
+      <ColoredMessage color="pink">元気です!</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </div>
   );
 };
