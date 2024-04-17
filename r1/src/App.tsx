c708651d879a29679bd58069f8a522347ce27243
koori <ice930ice@gmail.com>
2024-04-17 17:58:27 +0900

    axiosのｲﾝｽﾄｰﾙ（まとめ未完成）、p218まで完了

--- r1/src/App.tsx
+++ r1/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState}from "react";
+import {ListItem} from "./components/ListItem";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const App = () => {
+  const [users, setusers] = useState([]);
+
+  useEffect(()=> {
+    axios.get("https://example.com/users").then((res)=>{
+      setusers(res.data);
+    })
+  },[]);
+
+  return(
+    <div>
+      {users.map(user=>(<ListItem id={user.id} name={user.name} age={user.age}/>))}
     </div>
   );
-}
+};
 
 export default App;
